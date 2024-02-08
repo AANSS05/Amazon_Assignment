bea69c97baa0d6ff2d43df489c46e34fd9d51676
Adityakumar.a <assignment.hm05@gmail.com>
2024-02-08 17:27:56 +0530

    xpath modified

--- PageObjects/HomePage.py
+++ PageObjects/HomePage.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 class Home_Page:
     def __init__(self,driver):
         self.driver = driver
-        self.todays_deal_button_xpath = "//*[contains(text(),'Today's Deals')]"
+        self.todays_deal_button_xpath = """//a[contains(text(),"Today's Deals")]"""
 
     def click_on_todays_deal(self):
         self.driver.find_element(By.XPATH,self.todays_deal_button_xpath).click()

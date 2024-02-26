0c30f8f1a9c800940b9cc984be4ba36dfb919ef8
Saurabh Badave <saurabhbadave@gmail.com>
2024-02-26 19:02:45 +0530

    Scenario 4 Again Added

--- TestCases/test_Amazon.py
+++ TestCases/test_Amazon.py
@@ -1,6 +1,9 @@
 import pytest
 from PageObjects.HomePage import Home_Page
 from PageObjects.DealsPage import Deals_Page
+from PageObjects.ProductPage import Product_Page
+from PageObjects.ProductSelection import ProductSelection_Page
+from PageObjects.AddToCartPage import AddToCart_Page
 
 
 
@@ -24,3 +27,15 @@ from PageObjects.DealsPage import Deals_Page
 
         obj_ProductPage.print_product_details()
         obj_ProductPage.add_product_to_cart()
+
+
+    def test_scenario_04(self):
+        obj_ProductSelection = ProductSelection_Page(self.driver)
+        obj_ProductPage = Product_Page(self.driver)
+        obj_AddToCartPage = AddToCart_Page(self.driver)
+        obj_AddToCartPage.back_to_product_page()
+        obj_ProductPage.back_to_productSelection_page()
+        obj_ProductSelection.sort_items_lowest_price()
+        obj_ProductPage.print_product_details()
+        obj_ProductPage.check_eligibleFor_free_delivery_option()
+        obj_ProductPage.add_to_cart()

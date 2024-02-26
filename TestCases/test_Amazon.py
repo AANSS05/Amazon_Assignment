e580d71418d8c9d0e664fc554a995dabebaec184
AANSS05 <155517969+AANSS05@users.noreply.github.com>
2024-02-26 19:21:48 +0530

    Update test_Amazon.py

--- TestCases/test_Amazon.py
+++ TestCases/test_Amazon.py
@@ -6,29 +6,6 @@ from PageObjects.ProductSelection import ProductSelection_Page
 from PageObjects.AddToCartPage import AddToCart_Page
 
 
-
-
-    def test_scenario_03(self):
-        obj_ProductPage = Product_Page(self.driver)
-        obj_HomePage = Home_Page(self.driver)
-        obj_DealPage = Deals_Page(self.driver)
-        obj_ProductSelection = ProductSelection_Page(self.driver)
-
-        obj_HomePage.click_on_todays_deal()
-
-        obj_DealPage.click_on_mobile_and_accessories()
-        obj_DealPage.sort_discount_filter_by_high_to_low()
-        obj_DealPage.click_on_average_rating_4_and_up()
-        obj_DealPage.click_on_prime_deals_checkbox_and_verify_is_it_selected()
-        obj_DealPage.click_on_deal_of_the_day_deal_type()
-        obj_DealPage.click_on_card_with_highest_discount_percentage()
-        obj_ProductSelection.sorting_products_based_on_avg_customer_review()
-        obj_ProductSelection.clicking_on_product_with_highest_review_count()
-
-        obj_ProductPage.print_product_details()
-        obj_ProductPage.add_product_to_cart()
-
-
     def test_scenario_04(self):
         obj_ProductSelection = ProductSelection_Page(self.driver)
         obj_ProductPage = Product_Page(self.driver)

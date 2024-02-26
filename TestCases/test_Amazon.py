650d3fc42dfc008f8e00e4fa83765863044b4efa
Arshiya.tarannum <tarannum4@gmail.com>
2024-02-26 15:27:05 +0530

    deleted

--- TestCases/test_Amazon.py
+++ TestCases/test_Amazon.py
@@ -1,15 +1,3 @@
-import pytest
-from PageObjects.HomePage import Home_Page
-from PageObjects.DealsPage import Deals_Page
-from PageObjects.LoginPage import Login_Page
-from PageObjects.ProductPage import Product_Page
-from PageObjects.ProductSelection import ProductSelection_Page
-from PageObjects.AddToCartPage import AddToCart_Page
-from PageObjects.CheckoutPage import Checkout_page
-from selenium.webdriver.common.by import By
-
-@pytest.mark.usefixtures("setUp")
-class TestAllScenarios:
 
 
 

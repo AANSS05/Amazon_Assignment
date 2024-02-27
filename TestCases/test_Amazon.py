9397b9504d6071610329771accb0209bff04efc7
Syed0708 <syedmahaboob0708@gmail.com>
2024-02-27 11:49:36 +0530

    adding only scenario 5

--- TestCases/test_Amazon.py
+++ TestCases/test_Amazon.py
@@ -12,19 +12,7 @@ from selenium.webdriver.common.by import By
 
 @pytest.mark.usefixtures("setUp")
 class TestAllScenarios:
-    def test_Scenario_01(self):
-        obj_LoginPage = Login_Page(self.driver)
-        obj_LoginPage.click_on_signin_button_on_homepage()
-        obj_LoginPage.enter_email_address_text()
-        obj_LoginPage.click_on_continue_button()
-        obj_LoginPage.enter_password_text()
-        obj_LoginPage.click_on_signin_button()
-
-        obj_HomePage = Home_Page(self.driver)
-        obj_HomePage.Header_Footer()
-        obj_HomePage.card_details()
-
-
+    
     def test_scenario_05(self):
         obj_HomePage = Home_Page(self.driver)
         obj_AddToCartPage = AddToCart_Page(self.driver)
